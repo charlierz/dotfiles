c3d58327b776b51a274c2b6fd8441acabffa07e6
Carlos Rincon <carlos.rincon.zapata@gmail.com>
2025-03-09 02:34:14 +0100

    fix: slow down markdown preview

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -192,6 +192,7 @@ require('lazy').setup({
     build = 'cd app && npm install',
     init = function()
       vim.g.mkdp_filetypes = { 'markdown' }
+      vim.g.mkdp_refresh_slow = 1
     end,
     ft = { 'markdown' },
   },
