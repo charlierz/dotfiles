cc5cfbed755d09ae337f5ee244d880f6ee03bb7b
Carlos Rincon <carlos.r@affinidi.com>
2025-03-10 12:19:38 +0100

    feat: dart lsp

--- .config/nvim/lua/plugins/interface.lua
+++ .config/nvim/lua/plugins/interface.lua
@@ -57,42 +57,15 @@ return {
     ---@type ibl.config
     opts = {},
     config = function()
-      local highlight = {
-        'HighlightWhite',
-        'RainbowRed',
-        'RainbowYellow',
-        'RainbowBlue',
-        'RainbowOrange',
-        'RainbowGreen',
-        'RainbowViolet',
-        'RainbowCyan',
-      }
-
-      local hooks = require 'ibl.hooks'
-      -- create the highlight groups in the highlight setup hook, so they are reset
-      -- every time the colorscheme changes
-      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, 'HighlightWhite', { fg = '#a9b1d6' })
-        vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#E06C75' })
-        vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = '#E5C07B' })
-        vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#61AFEF' })
-        vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = '#D19A66' })
-        vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = '#98C379' })
-        vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = '#C678DD' })
-        vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = '#56B6C2' })
-      end)
-      vim.g.rainbow_delimiters = { highlight = highlight }
       require('ibl').setup {
         indent = {
-          char = '▏', -- This is a slightly thinner char than the default one, check :help ibl.config.indent.char
+          char = '▏',
         },
-        scope = { highlight = highlight },
+        scope = {
+          show_start = false,
+          show_end = false,
+        },
       }
-      hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
     end,
-  },
-
-  { -- delimiters
-    'HiPhish/rainbow-delimiters.nvim',
   },
 }
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -278,4 +278,13 @@ return {
       }
     end,
   },
+  {
+    'nvim-flutter/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+    config = true,
+  },
 }
