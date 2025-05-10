15dab197eafe169cd6bc109bb5095c3464171356
charlierz <carlos.rincon.zapata@gmail.com>
2025-05-11 01:38:44 +0200

    feat: run zellij on fish startup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     fish_config theme choose "Dracula"
     fish_vi_key_bindings
+    eval (zellij setup --generate-auto-start fish | string collect)
 end
 
 set -g fish_greeting
