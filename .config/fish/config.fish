f76559b2165c2406e6ed256eb07eb0985e3ba296
charlierz <carlos.rincon.zapata@gmail.com>
2025-05-11 01:23:26 +0200

    feat: gnome and fish adjustments

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,22 @@
 if status is-interactive
     fish_config theme choose "Dracula"
+    fish_vi_key_bindings
 end
 
 set -g fish_greeting
 
-export EDITOR=nvim
-export VISUAL="$EDITOR"
+set -gx EDITOR nvim
+set -gx VISUAL "$EDITOR"
 
 fzf --fish | source
 zoxide init fish | source
 
-abbr -a g lazygit
-abbr -a e yazi
-abbr -a j zellij
-abbr -a v nvim
+alias g="lazygit"
+alias e="yazi"
+alias j="zellij"
+alias v="nvim"
+
+if test (uname -s) = "Linux"
+    fish_add_path /usr/local/go/bin
+    fish_add_path /home/charlierz/.local/share/pnpm
+end
