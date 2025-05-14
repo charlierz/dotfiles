330456ce2f220a91fee211e177f23effe136d577
Carlos Rincon <carlos.r@affinidi.com>
2025-05-14 13:19:24 +0200

    fix: aider on macos and fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,24 @@
-# macOS path workaround
-fish_add_path /opt/homebrew/bin
+if test (uname -s) = "Linux"
+    # --- linux settings ---
+    fish_add_path /usr/local/go/bin
+    fish_add_path /home/charlierz/.local/share/pnpm
+else if test (uname -s) = "Darwin"
+    # --- macOS settings ---
+    fish_add_path "$HOME/Development/flutter"
+    fish_add_path "$HOME/Development/flutter/bin"
+    fish_add_path "$HOME/.pub-cache/bin"
+    fish_add_path "$HOME/bin/aws-accounts"
+    fish_add_path "$HOME/bin/robq-tools"
+    fish_add_path "$HOME/.local/bin" # For aider
+    fish_add_path /opt/homebrew/bin
+    set -gx AFFINIDI_SERVICE_TS /.../affinidi-service-ts
+    set -gx GENESIS_LIBS /.../libs
+    set -gx AWS_PROFILE affinidi-elements-dev-dev-portal-bff:Developer
+    set -gx XDG_CONFIG_HOME "$HOME/.config" # For lazygit config location
+    alias pj 'npx projen'
+    alias bd './bin/dev.js'
+    alias br './bin/run.js'
+end
 
 if status is-interactive
     fish_config theme choose "Dracula"
@@ -7,7 +26,7 @@ if status is-interactive
     eval (zellij setup --generate-auto-start fish | string collect)
 end
 
-set -g fish_greeting
+set -g fish_greeting # disable fish greeting
 
 set -gx EDITOR nvim
 set -gx VISUAL "$EDITOR"
@@ -20,23 +39,3 @@ alias e="yazi"
 alias j="zellij"
 alias v="nvim"
 
-if test (uname -s) = "Linux"
-    # --- linux settings ---
-    fish_add_path /usr/local/go/bin
-    fish_add_path /home/charlierz/.local/share/pnpm
-
-else if test (uname -s) = "Darwin"
-    # --- macOS settings ---
-    fish_add_path "$HOME/Development/flutter"
-    fish_add_path "$HOME/Development/flutter/bin"
-    fish_add_path "$HOME/.pub-cache/bin"
-    fish_add_path "$HOME/bin/aws-accounts"
-    fish_add_path "$HOME/bin/robq-tools"
-    set -gx AFFINIDI_SERVICE_TS /.../affinidi-service-ts
-    set -gx GENESIS_LIBS /.../libs
-    set -gx AWS_PROFILE affinidi-elements-dev-dev-portal-bff:Developer
-    set -gx XDG_CONFIG_HOME "$HOME/.config" # For lazygit config location
-    alias pj 'npx projen'
-    alias bd './bin/dev.js'
-    alias br './bin/run.js'
-end
