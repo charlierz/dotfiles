face9d8a51822e033ff3d94d8897606f63c8bbab
charlierz <carlos.rincon.zapata@gmail.com>
2025-06-04 20:56:14 +0200

    chore: simplify

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -168,7 +168,7 @@ vim.opt.rtp:prepend(lazypath)
 --    :Lazy
 --  To update plugins you can run
 --    :Lazy update
-require('lazy').setup({
+require('lazy').setup {
 
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
@@ -204,27 +204,7 @@ require('lazy').setup({
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
   { import = 'plugins' },
-}, {
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
-})
+}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
